34bd1240513d682d09a46bf8fed74b86bc53cc24
Marie-Lou Denis <marielou33690@gmail.com>
2021-03-23 09:10:18 -0400

    version finale

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/firestore';
 
 // Configuration (mettez-y les vôtres !)
 const firebaseConfig = {
-  apiKey: "AIzaSyBLZ1MjGR-9naCsqeMjQIppEwkM9qZVMUY",
-  authDomain: "marielou-react.firebaseapp.com",
-  projectId: "marielou-react",
-  storageBucket: "marielou-react.appspot.com",
-  messagingSenderId: "662884818716",
-  appId: "1:662884818716:web:3b56dcf4df22f388e064d9"
+ 
 };
 
 // Initialiser Firebase
